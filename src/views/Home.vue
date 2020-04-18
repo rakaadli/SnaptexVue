d8c1c1d43afa7e341ca13bc79c47e1c89fec67bb
rakaadli <34180704+rakaadli@users.noreply.github.com>
2020-04-18 16:04:19 +0700

    second commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderSnaptex />
+    <HeroSnaptex />
+    <WomanBannerSnaptex />
+    <InstaSnaptex />
+    <PartnerSnaptex />
+    <FooterSnaptex />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSnaptex from "@/components/HeaderSnaptex.vue";
+import HeroSnaptex from "@/components/HeroSnaptex.vue";
+import WomanBannerSnaptex from "@/components/WomanBannerSnaptex.vue";
+import InstaSnaptex from "@/components/InstaSnaptex.vue";
+import PartnerSnaptex from "@/components/PartnerSnaptex.vue";
+import FooterSnaptex from "@/components/FooterSnaptex.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    HeaderSnaptex,
+    HeroSnaptex,
+    WomanBannerSnaptex,
+    InstaSnaptex,
+    PartnerSnaptex,
+    FooterSnaptex
+  }
+};
+</script>
